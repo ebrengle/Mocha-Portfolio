34bb5415fae9cfe180f9303d529bf6078c248583
Eric Brengle <eric.brengle@gmail.com>
2015-02-05 12:08:40 -0600

    added a whole mess of practice tests

--- test/spec/main_test.js
+++ test/spec/main_test.js
@@ -3,7 +3,35 @@
 describe('test suite', function () {
   it('should assert true', function() {
     true.should.be.true;
-    false.should.be.true;
+    false.should.be.false;
+  });
+});
+
+describe('hello', function () {
+  it('should return world', function () {
+    hello().should.equal('world');
+  });
+});
+
+describe('getStock', function () {
+  it('should return the stock object', function(done){
+    getStock('AAPL', function (stock) {
+      stock.Name.should.equal('Apple Inc');
+      done();
+    });
+
+   //getStock('MSFT', function (stock) {
+     //stock.Name.should.equal('Microsoft Corp');
+   //});
+  });
+});
+
+describe('addStockToTable', function () {
+  it('should use stock info to append to table', function () {
+    var stock = {Name: "SuperCorp", Symbol: "SCRP", LastPrice: "11.34" };
+    $('tr').length.should.equal(0);
+    addStockToTable(stock);
+    $('tr').length.should.equal(1);
   });
 });
 
