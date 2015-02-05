c8551cab03153e9db3d0e7cacb9bf05e8799d3f7
Eric Brengle <eric.brengle@gmail.com>
2015-02-05 16:03:46 -0600

    async

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,18 @@
-function addStockToTable () {
+function addStockToTable (stock) {
   var $row = $('<tr></tr>');
+  $row.append('<td>' + stock.Name + '</td>');
+  $row.append('<td>' + stock.Symbol + '</td>');
+  $row.append('<td>' + stock.LastPrice + '</td>');
+
+
   $('tbody').append($row);
+
+  return $row;
 }
 
 function getStock(symbol, cb){
   var url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=' + symbol;
-  $.get(url, function(res){ 
+  $.get(url, function(res){
     return cb(res);
   }, 'jsonp');
 }
--- test/spec/main_test.js
+++ test/spec/main_test.js
@@ -27,11 +27,22 @@ describe('getStock', function () {
 });
 
 describe('addStockToTable', function () {
-  it('should use stock info to append to table', function () {
+  it('should add row to table', function () {
     var stock = {Name: "SuperCorp", Symbol: "SCRP", LastPrice: "11.34" };
     $('tr').length.should.equal(0);
     addStockToTable(stock);
     $('tr').length.should.equal(1);
   });
+  it('it should use stock data in the append row', function () {
+    var stock = {Name: "SuperCorp", Symbol: "SCRP", LastPrice: "11.34" },
+      $row = addStockToTable(stock),
+      $tds = $row.find('td');
+
+    $tds.length.should.equal(3);
+    $($tds[0]).text().should.equal('SuperCorp');
+    $($tds[1]).text().should.equal('SCRP');
+    $($tds[2]).text().should.equal('11.34');
+  
+  })
 });
 
